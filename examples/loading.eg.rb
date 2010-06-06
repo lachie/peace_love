f28b5b2363adfd66a3d0fec969ba37cde467730e
lachie <lachiec@gmail.com>
2010-06-06 20:40:26 +1000

    fleshing out ideas - proved concept

--- examples/loading.eg.rb
+++ examples/loading.eg.rb
@@ -1,31 +1,140 @@
 require 'eg.helper'
 require 'angry_hash'
 
-eg.setup do
-  @col = $db['loading']
-  @col.remove()
-end
 
-module Customiser
-  def another_thing
-    self.somesuch.upcase
+module PeaceLove
+  class << self
+    attr_accessor :db
+
+    def collections
+      @collections ||= {}
+    end
+
+    def [](collection_name)
+      collections[collection_name.to_s] ||= PeaceLove::Collection.new( db[collection_name] )
+    end
+  end
+
+  class Collection
+    attr_accessor :mixin
+
+    def initialize(collection)
+      @collection = collection
+    end
+
+    def build(seed={})
+      __extend( AngryHash[ seed ] )
+    end
+
+    def find_one(*args)
+      __extend( AngryHash[ @collection.find_one(*args) ] )
+    end
+
+    def find(*args,&block)
+      if block_given?
+        find(*args) {|cursor| yield __extend_cursor(cursor)}
+      else
+        __extend_cursor(find(*args))
+      end
+    def 
+
+    def __extend_cursor(cursor)
+    end
+
+    def __extend(hash)
+      hash.extend mixin if mixin
+      hash
+    end
+  end
+
+  module Doc
+    class << self
+      def included(base)
+        base.extend ClassMethods
+
+        base.module_eval do
+          def self.extend_object(obj)
+            Doc.mark_extension(obj,self)
+            super
+          end
+        end
+      end
+
+      def extension_registry
+        @extension_registry ||= Hash.new {|h,k| h[k] = []}
+      end
+      def mark_extension(obj,with)
+        extension_registry[obj.__id__] << with
+      end
+
+      def mixin_registry
+        @mixin_registry ||= Hash.new {|h,k| h[k] = {}}
+      end
+      def mixin(target_class,field,mod)
+        mixin_registry[target_class][field.to_s] = mod
+      end
+
+      def mixin_to(parent_obj,field,obj)
+        extensions = extension_registry[parent_obj.__id__]
+
+        mixins = mixin_registry.values_at(*extensions)
+
+        mixins.map {|m| m[field.to_s]}.flatten.compact.each {|mod| obj.extend mod}
+        obj
+      end
+    end
+
+    def [](key)
+      Doc.mixin_to(self,key,super)
+    end
+
+    module ClassMethods
+      def sub_doc(field,mod)
+        Doc.mixin(self,field,mod)
+      end
+    end
   end
 end
 
-eg 'loading objects' do
-  @col.insert(:somesuch => 'and overmore')
-
-  h = AngryHash[ @col.find(:somesuch => 'and overmore').first ]
-  h.extend Customiser
-
-  Show( h.somesuch )
-  Show( h.another_thing )
-  Show( h )
+eg.setup do
+  PeaceLove.db = $db
+end
 
 
+module Kind
+  def claws; "dainty" end
 
-  #PeaceLove.db = $db
-  #PeaceLove['loading']
+  def for_kids?
+    fictional? && cartoon?
+  end
+end
+
+module Bear
+  include PeaceLove::Doc
+  sub_doc :kind, Kind
+
+  def claws; "woah" end
+  def liver
+    super.upcase
+  end
+end
+
+eg 'loading object' do
+  $db['bears'].remove()
+  $db['bears'].insert(:name => 'yogi', :liver => 'pure', :kind => {:fictional => true, :cartoon => true})
+
+  PeaceLove['bears'].mixin = Bear
+  yogi = PeaceLove['bears'].find_one(:name => 'yogi')
+  Show(yogi.claws)
+  Show(yogi.name)
+  Show(yogi.liver)
+  Show(yogi.kind.for_kids?)
+end
+
+eg 'blank doc' do
+  PeaceLove['bears'].mixin = Bear
+  yogi = PeaceLove['bears'].build
+  yogi.claws
 end
 
 eg 'saving object' do
