a81e9c868806c36f8a17afe056034de79c39d670
lachie <lachiec@gmail.com>
2010-07-14 10:54:19 +1000

    succinct BSON obj id output in YAML, for exemplor's Show()

--- examples/eg.helper.rb
+++ examples/eg.helper.rb
@@ -18,4 +18,13 @@ class Object
 end
 
 require 'mongo'
+
+class BSON::ObjectID
+  def to_yaml(opts = {})
+    YAML.quick_emit(nil, opts) do |out|
+      out.scalar(nil, "oid(#{to_s})", :plain)
+    end
+  end
+end
+
 $db = Mongo::Connection.new.db('sample-db')
